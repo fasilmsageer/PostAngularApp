8b2d8e41e6253f08d5bd5de1d38cf82dd19c358e
fasilmsageer <fasilmsageer@gmail.com>
2022-11-28 20:10:17 +0530

    cred

--- src/app/addpost/addpost.component.ts
+++ src/app/addpost/addpost.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AddpostComponent {
 
+  userId=""
+  id=""
+  title=""
+  body=""
+  
+
+  readValues=()=>
+  {
+    let data:any=
+      {
+        "userId":this.userId,
+        "id":this.id,
+        "title":this.title,
+        "body":this.body     }
+
+        console.log(data)
+  }
+
 }
